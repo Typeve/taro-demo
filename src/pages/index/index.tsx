2f9b1222272e4f5699ee34811953353a3dd85ae0
wxq <wxq@yeeaf.com>
2023-07-14 09:16:28 +0800

    初始化

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "@tarojs/components";
+import Layout from "../../components/Layout";
+
+export default function Index() {
+  return (
+    <View>
+      <Layout>测试</Layout>
+      首页
+    </View>
+  );
+}
---
+++ src/pages/my/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "@tarojs/components";
+import Layout from "../../components/Layout";
+
+export default function My() {
+  return (
+    <View>
+      <Layout>我的</Layout>
+      我的页面
+    </View>
+  );
+}
